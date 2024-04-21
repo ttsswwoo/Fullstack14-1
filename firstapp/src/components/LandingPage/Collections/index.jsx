bfabf6957baabeb8ceea41fa114a0d6575e14690
ttsswwoo <farzonaanvarovnas@example.com>
2024-04-21 20:25:35 +0500

    git pull

---
+++ firstapp/src/components/LandingPage/Collections/index.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import Ownership from "./Ownership"
+import Leaderboard from "./Leaderboard"
+import './style.scss'
+
+function Collections() {
+    return (
+        <>
+
+            <div className="container">
+                <h1>Featured Collections</h1>
+            </div>
+            <div className="cards">
+                <div className="right">
+                    <h2>ExBoot #1</h2>
+                    <p>Description</p>
+                    <p>We would like to present you The Exboot 3D - Watching you~</p>
+                    <div>
+                        <p>Current Bid</p>
+                        <p>Current Bid</p>
+                    </div>
+                </div>
+                <div className="left">
+
+                </div>
+            </div>
+            <div className="cards">
+                <div className="right">
+                    <h2>ExBoot #1</h2>
+                    <p>Description</p>
+                    <p>We would like to present you The Exboot 3D - Watching you~</p>
+                    <div>
+                        <p>Current Bid</p>
+                        <p>Current Bid</p>
+                    </div>
+                </div>
+                <div className="left">
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Collections;
--- firstapp/src/components/LandingPage/index.jsx
+++ firstapp/src/components/LandingPage/index.jsx
@@ -1,9 +1,4 @@
-// /* eslint-disable no-unused-vars */
-// import Salin1 from '../../../assets/imgs/Salin1.png'
-// import Salin2 from '../../../assets/imgs/Salin2.png'
-// import Salin3 from '../../../assets/imgs/Salin3.png'
-// import Salin4 from '../../../assets/imgs/Salin4.png'
-
+/* eslint-disable no-unused-vars */
 import Ownership from "./Ownership"
 import Leaderboard from "./Leaderboard"
 import "./style.scss"
@@ -15,23 +10,19 @@ function LandingPage() {
             <Leaderboard />
             <div className="logo">
                 <h1>Trending Art 🔥</h1>
-                <p>Discover more</p>    
+                <p>Discover more</p>
             </div>
             <div className="cards">
                 <div className="card-1">
-                    {/* <img src={Salin1} width={"150px"} alt="" /> */}
                     <p>ExBoot #1</p>
                 </div>
                 <div className="card-2">
-                    {/* <img src={Salin2} width={"150px"} alt="" /> */}
                     <p>ExBoot #2</p>
                 </div>
                 <div className="card-3">
-                    {/* <img src={Salin3} width={"150px"} alt="" /> */}
                     <p>Future of Polygon X</p>
                 </div>
                 <div className="card-4">
-                    {/* <img src={Salin4} width={"150px"} alt="" /> */}
                     <p>Blue Wave #2</p>
                 </div>
             </div>
